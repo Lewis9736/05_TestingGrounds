be50db7b3df6089660970de4263c7c5a5ed87085
DARKABYSS\Lucan <lewis9736_pearson@hotmail.co.uk>
2017-12-31 11:23:19 +0000

    C++ AI Behaviour Tree Tasks

---
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,11 @@
+// Lone Wolf Studio
+
+#include "S05_TestingGrounds.h"
+#include "ChooseNextWaypoint.h"
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	EBTNodeResult::Succeeded;
+}
+
